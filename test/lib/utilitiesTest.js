8b93029179ac0a84915a8c5ad627eee23862eca2
Marc Sygrove <tigrou.m@gmail.com>
2016-11-03 16:20:56 +0000

    added test for the main functions

--- test/lib/utilitiesTest.js
+++ test/lib/utilitiesTest.js
@@ -2,6 +2,7 @@
 
 var utilities = require('../../lib/utilities');
 var _ = require('lodash');
+var Q = require('q');
 
 describe('utilities', function() {
 
@@ -10,27 +11,27 @@ describe('utilities', function() {
   });
 
   describe('#collectIncludes', function() {
-    it('here', function() {
+    it('should collect the include and add it to the stateObject', function() {
       var stateObject = {};
       _.set(stateObject, 'request.data.query.include', 'foo');
-      var expected = stateObject;
+
+      var expected = _.cloneDeep(stateObject);
       expected.includes = ['foo'];
 
-      expect(utilities.collectIncludes(stateObject)).to.eventually.deep.equal(expected);
+      return expect(utilities.collectIncludes(stateObject)).to.eventually.deep.equal(expected);
     });
 
-    it('here2', function() {
+    it('should return foo and bar in an array on the stateObject', function() {
       var stateObject = {};
       _.set(stateObject, 'request.data.query.include', 'foo,bar');
       var expected = stateObject;
       expected.includes = ['foo', 'bar'];
 
-      expect(utilities.collectIncludes(stateObject)).to.eventually.deep.equal(expected);
+      return expect(utilities.collectIncludes(stateObject)).to.eventually.deep.equal(expected);
     });
 
-    it('here3', function() {
-      var stateObject = {};
-      expect(utilities.collectIncludes(stateObject)).to.eventually.deep.equal(stateObject);
+    it('should return early as no include found', function() {
+      return expect(utilities.collectIncludes({})).to.eventually.deep.equal({});
     });
   });
 
@@ -38,33 +39,140 @@ describe('utilities', function() {
   describe('#getResources', function() {
 
     beforeEach(function() {
-      sandbox.stub(utilities, '_getResourcesLinks').returns('RESOURCE_LINKS');
-      sandbox.stub(utilities, '_buildHref').returns('HREF2');
+      sandbox.stub(utilities, '_buildHref').returns('HREF');
     });
 
-    it('here', function() {
+    it('should return early as there are no ids on the resource', function() {
+      var stateObject = {
+        resources: 'RESOURCES'
+      };
+
+      return expect(utilities.getResources(stateObject)).to.eventually.deep.equal(stateObject);
+    });
+
+    it('should build a href for the resource', function() {
       var stateObject = {
         resources: [
           {
-            id: 'ID2'
+            id: 'ID'
           }
         ]
       };
       _.set(stateObject, 'request.route.settings.bind.resourceName', 'TYPE');
 
-      var expected = {
+      var expected = _.cloneDeep(stateObject);
+      expected.resources = [
+        {
+          id: 'ID',
+          href: 'HREF'
+        }
+      ];
+
+      return expect(utilities.getResources(stateObject)).to.eventually.deep.equal(expected);
+    });
+  });
+
+
+  describe('#getSubResources', function() {
+    it('should add a sub resource', function() {
+      var stateObject = {
         resources: [
           {
             id: 'ID',
-            href: 'HREF'
+            links: {
+              LINK1: {
+                ids: ['LINK_ID1'],
+                type: 'LINK1'
+              }
+            }
+          }
+        ],
+        includes: ['LINK1']
+      };
+
+      var expected = _.cloneDeep(stateObject);
+      expected.subResourceRequests = {
+        LINK1: {
+          ids: ['LINK_ID1']
+        }
+      };
+
+      return expect(utilities.getSubResources(stateObject)).to.eventually.deep.equal(expected);
+    });
+
+    it('should not attach any sub resources as nothing is included', function() {
+      var stateObject = {
+        resources: [
+          {
+            id: 'ID',
+            links: {
+              LINK1: {
+                ids: ['LINK_ID1'],
+                type: 'LINK1'
+              }
+            }
           }
         ]
       };
 
-      expect(utilities.getResources(stateObject)).to.eventually.deep.equal(expected);
+      return expect(utilities.getSubResources(stateObject)).to.eventually.deep.equal(stateObject);
+    });
+  });
+
+
+
+  describe('#resolveLinkedData', function() {
+
+    beforeEach(function() {
+      sandbox.stub(utilities, '_requestSubResource').returns(Q.resolve({
+        SUBRESOURCE1: [
+          {
+            linked: 'SUBRESOURCE1_LINKED',
+            meta: 'SUBRESOURCE1_META'
+          }
+        ]
+      }));
     });
 
 
-  });
+    it('should return early as no sub reources are provided', function() {
 
+      return expect(utilities.resolveLinkedData({})).to.eventually.be.undefined();
+    });
+
+
+    it('should attach the sub reources as linked', function() {
+      var stateObject = {
+        request: {
+          response: {
+            source: {
+              result: 'RESULT'
+            }
+          }
+        },
+        subResourceRequests: {
+          SUBRESOURCE1: {
+            linked: 'SUBRESOURCE1_LINKED'
+          }
+        }
+      };
+
+      var expected = {
+        result: 'RESULT',
+        linked: {
+          SUBRESOURCE1: [
+            {
+              linked: 'SUBRESOURCE1_LINKED',
+              meta: 'SUBRESOURCE1_META'
+            }
+          ]
+        }
+      };
+
+      return utilities.resolveLinkedData(stateObject)
+      .then(function() {
+        return expect(stateObject.request.response.source).to.be.deep.equal(expected);
+      });
+    });
+  });
 });
